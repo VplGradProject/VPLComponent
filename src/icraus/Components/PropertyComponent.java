109fe42e695ce59050004122183893d8d7e73ab2
Icarus <Mohamed.khaled.courses@gmail.com>
2018-06-09 13:48:08 +0200

    v0.802

---
+++ src/icraus/Components/PropertyComponent.java
@@ -0,0 +1,17 @@
+package icraus.Components;
+
+public class PropertyComponent extends Component {
+    
+    public PropertyComponent(){
+
+    }
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+}
